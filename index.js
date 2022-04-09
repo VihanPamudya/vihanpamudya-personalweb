be81adf401dcbaa67d0ef66339aa5256fbaa1f3c
vihan pamudya <vihanpamudya12@gmail.com>
2022-04-10 01:06:46 +0530

    navlink updated

--- index.js
+++ index.js
@@ -9,3 +9,33 @@ const showMenu = (toggleId,navId) =>{
 }
 
 showMenu('nav-toggle','nav-menu')
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+
+function scrollActive(){
+    const scrolly = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrolly > sectionTop && scrolly <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
