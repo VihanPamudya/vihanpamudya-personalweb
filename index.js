442ecbecabea24a96d12d77c8993a549e5f532db
vihan pamudya <vihanpamudya12@gmail.com>
2022-04-11 00:59:16 +0530

    major changes

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
-const showMenu = (toggleId,navId) =>{
-    const toggle = document.getElementById(toggleId), nav = document.getElementById(navId)
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click',()=>{
-            nav.classList.toggle('show-menu')
-        })
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrolly = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrolly > sectionTop && scrolly <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
     }
+  });
 }
 
-showMenu('nav-toggle','nav-menu')
+window.addEventListener("scroll", scrollActive);
 
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+function scrollHeader() {
+  const header = document.getElementById("header");
+  if (this.scrolly >= 200) header.classList.add("scroll-header");
+  else header.classList.remove("scroll-header");
 }
+window.addEventListener("scroll", scrollHeader);
 
-navLink.forEach(n => n.addEventListener('click',linkAction))
-
-const sections = document.querySelectorAll('section[id]')
-
-
-function scrollActive(){
-    const scrolly = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
-
-        if(scrolly > sectionTop && scrolly <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  if (this.scrolly >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
 }
-
-window.addEventListener('scroll', scrollActive)
-
-function scrollHeader(){
-    const header = document.getElementById('header')
-    if(this.scrolly >= 200) header.classList.add('scroll-header')
-    else header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollTop);
